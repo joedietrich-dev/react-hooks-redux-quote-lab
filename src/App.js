b7da330239f64ab7dea4ddda11104ace8894c5b7
joedietrich-dev <joe@joedietrich.net>
2022-01-07 22:46:46 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
+import QuoteForm from "./features/quotes/QuoteForm";
+import Quotes from "./features/quotes/Quotes";
 
 function App() {
   return (
     <div className="container-fluid">
-      <div
-        className="row title justify-content-center"
-        style={{ paddingTop: "12px" }}
-      >
+      <div className="row title justify-content-center" style={{ paddingTop: "12px" }}>
         <h1>Quote Maker</h1>
       </div>
       <hr />
-      {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+      <QuoteForm />
+      <Quotes />
     </div>
   );
 }
